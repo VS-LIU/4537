fe89c05cc914380b49329b0951faac5fd916c45d
VS-LIU <vctl95@gmail.com>
2024-09-14 22:16:22 -0700

    Create note class

---
+++ Lab1/js/note.js
@@ -0,0 +1,22 @@
+/**
+ * COMP4537 - Lab 1
+ * Name: Victor Liu #A00971668 - Set C
+ * Date: 2024-09-15
+ * 
+ * note.js
+ */ 
+
+class Note {
+    constructor(title, body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    getBody() {
+        return this.body;
+    }
+}

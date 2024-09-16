e566502d44f5ad3626e0a59c90ce5c458bd692e3
VS-LIU <vctl95@gmail.com>
2024-09-15 21:32:53 -0700

    Clean

--- Lab1/js/writer.js
+++ Lab1/js/writer.js
@@ -4,35 +4,16 @@
  * Date: 2024-09-15
  * 
  * writer.js
- */ 
+ */
 
 
 
-
-
-const msg_notSupported = "Sorry web Storage is not supported!";
-const msg_key = "hidden secret";
-const msg_written="A piece of data was written in local storage for the key:"
-;
 if (typeof (Storage) == "undefined") {
-document.write(msg_notSupported);
-window.stop();
+    document.write(msg_notSupported);
+    window.stop();
 }
-
 
 const noteManager = new NoteManager();
 const buttonCreateNote = new ButtonCreateNote(noteManager);
 const buttonSaveNotes = new ButtonSaveNotes(noteManager);
 buttonSaveNotes.buttonAddEventListener();
-
-// function initializeNotes() {
-//     const savedNotes = JSON.parse(localStorage.getItem("notes")) || [];
-//     savedNotes.forEach(noteData => {
-//         // Create and add note based on saved data
-//         const note = new Note(noteData.id, noteData.top, noteData.left);
-//         note.text = noteData.text; // Restore the text content
-//         noteManager.notes.push(note);
-//         document.getElementById("notes-container").appendChild(note.getNoteElement());
-//     });
-// }
-// initializeNotes();

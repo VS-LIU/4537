6aa368ff751d1c8e0c66aabdf3c65b6ea1370b0d
VS-LIU <vctl95@gmail.com>
2024-09-15 21:31:44 -0700

    Add string constants

---
+++ Lab1/lang/messages/en/user.js
@@ -0,0 +1,10 @@
+/**  
+ * COMP4537 - Lab 1
+ * Name: Victor Liu #A00971668 - Set C
+ * Date: 2024-09-15
+*/ 
+
+
+const msg_notSupported = "Sorry web Storage is not supported!";
+const msg_key = "hidden secret";
+const msg_written = "A piece of data was written in local storage for the key:";

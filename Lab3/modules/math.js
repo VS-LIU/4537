560178622cded9f5c5f450475d4d43878b863af2
VS-LIU <vctl95@gmail.com>
2024-09-29 14:02:53 -0700

    Add Part A

---
+++ Lab3/modules/math.js
@@ -0,0 +1,5 @@
+const PI = 3.14159265359;
+exports.area = 
+function (r) {
+    return PI * r * r;
+};
---
+++ Lab3/testWithModules.js
@@ -0,0 +1,8 @@
+// const PI=3.14
+// let r = 10;
+// let area = PI * r * r;
+// console.log(area);
+
+
+const mo = require('./modules/math');
+console.log(mo.area(2));

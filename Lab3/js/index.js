8201c89796467f8cbb22a2c3a0f0f25ad2f3a5ae
VS-LIU <vctl95@gmail.com>
2024-09-29 21:06:50 -0700

    Update front end with notes

--- Lab3/js/index.js
+++ Lab3/js/index.js
@@ -16,11 +16,26 @@ const currentProtocol = window.location.protocol;
 const currentPort = window.location.port; // Get the port number
 
 // Construct the full URL, including the port if it's present
-const fullUrl = `${currentProtocol}//${currentDomain}${currentPort ? `:${currentPort}` : ''}/COMP4537/labs/3/getDate?name=PLACEHOLDER`;
+const fullUrlGetDate = `${currentProtocol}//${currentDomain}${currentPort ? `:${currentPort}` : ''}/COMP4537/labs/3/getDate?name=PLACEHOLDER`;
+const fullUrlRead = `${currentProtocol}//${currentDomain}${currentPort ? `:${currentPort}` : ''}/COMP4537/labs/3/readFile/file.txt`;
+const fullUrlWrite = `${currentProtocol}//${currentDomain}${currentPort ? `:${currentPort}` : ''}/COMP4537/labs/3/writeFile/?text=PLACEHOLDER_TEXT`;
+
+console.log("GetDate URL:", fullUrlGetDate);
+console.log("Read URL:", fullUrlRead);
+console.log("Write URL:", fullUrlWrite);
+
 
 // Update the href of the anchor element
 const dynamicUrlElement = document.getElementById('dynamic-url');
-dynamicUrlElement.href = fullUrl;
+const dynamicUrlReadElement = document.getElementById('url-read');
+const dynamicUrlWriteElement = document.getElementById('url-write');
+
+dynamicUrlElement.href = fullUrlGetDate;
+dynamicUrlReadElement.href = fullUrlRead;
+dynamicUrlWriteElement.href = fullUrlWrite;
 
 // Replace the innerHTML of the <code> element to keep styling
-dynamicUrlElement.innerHTML = `<code>https://${currentDomain}${currentPort ? `:${currentPort}` : ''}/COMP4537/labs/3/getDate?name=<span class="make-red">PLACEHOLDER</span></code>`;
+dynamicUrlElement.innerHTML = `<code>${currentProtocol}//${currentDomain}${currentPort ? `:${currentPort}` : ''}/COMP4537/labs/3/getDate?name=<span class="make-red">PLACEHOLDER</span></code>`;
+dynamicUrlReadElement.innerHTML = `<code>${currentProtocol}//${currentDomain}${currentPort ? `:${currentPort}` : ''}/COMP4537/labs/3/readFile/file.txt</code>`;
+dynamicUrlWriteElement.innerHTML = `<code>${currentProtocol}//${currentDomain}${currentPort ? `:${currentPort}` : ''}/COMP4537/labs/3/writeFile/?text=<span class="make-red">PLACEHOLDER_TEXT</span></code>`;
+

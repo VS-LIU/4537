3c044027ac4befc8c7e1142e7ae7eb301255c88f
VS-LIU <vctl95@gmail.com>
2024-10-11 11:19:36 -0700

    Add Lab4

---
+++ Lab4/ServerClient/lang/en/en.js
@@ -0,0 +1,12 @@
+/**
+ * COMP4537 - Lab 4
+ * Names: Andy Lau #A01192350 - Victor Liu #A00971668
+ * Date: 2024-10-20
+ *
+ * en.js
+ */
+
+const RESPONSE_STATUS = (status) => `Response status: ${status}`;
+const STATUS_405 = (status) => `${status} (cannot access the requested page)`;
+
+export default { RESPONSE_STATUS, STATUS_405 };
